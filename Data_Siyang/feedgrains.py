efff09dd2d8c94c0b5cc3866e33d05d23bd51cea
FengSiyang <1242876589@qq.com>
2020-05-31 13:15:54 -0400

    .

--- Data_Siyang/feedgrains.py
+++ Data_Siyang/feedgrains.py
@@ -1,7 +1,13 @@
 # %%
 import numpy as np
 import pandas as pd
-import matplotlib
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+sns.set(style="white")
+sns.set(style="whitegrid", color_codes=True)
 
 
 # %%
@@ -15,8 +21,6 @@ feedgrains.describe(include='all')
 # %%
 feedgrains.info(verbose=True)
 
-# %%
-feedgrains.columns
 
 # %%
 print('SC_Group_Desc : \n', feedgrains['SC_Group_Desc'].unique(), '\n')
@@ -29,5 +33,33 @@ print('SC_Frequency_Desc: \n', feedgrains['SC_Frequency_Desc'].unique(), '\n')
 print('Timeperiod_Desc: \n', feedgrains['Timeperiod_Desc'].unique()), '\n'
 
 
+
+# %%
+price_annual = feedgrains[(feedgrains['SC_Group_Desc'] == 'Prices') & (feedgrains['SC_Frequency_Desc'] == 'Annual')]
+
+
+# %%
+price_annual_corn = price_annual[feedgrains['SC_GroupCommod_Desc'] == 'Oats']
+
+# %%
+X_train, X_test, y_train, y_test = train_test_split(price_annual_corn['Year_ID'].values.reshape(-1, 1), price_annual_corn['Amount'], test_size=0.2, random_state=101)
+corn_linear = linear_model.LinearRegression()
+corn_linear.fit(X_train, y_train)
+
+
+# %%
+y_predict = corn_linear.predict(X_test)
+
+
+# %%
+y_predict.shape
+
+
+# %%
+plt.scatter(price_annual_corn['Year_ID'], price_annual_corn['Amount'])
+plt.title("Annual Price and Year (1860 - 2020)")
+plt.xlabel("Year")
+plt.ylabel("Annual Price")
+plt.show()
 
 # %%
